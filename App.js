8ea4a926aeed352c4dd0382a67fec8a064b82032
svidunovichromanv <svidunovichromanv@gmail.com>
2018-11-12 22:58:22 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import PagesRouter from './pages/PagesRouter';
+import PagesLinks from './pages/PagesLinks';
+import './main.css';
+
+// если необходимо, вид сборки можно проверить в коде:
+// if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV !== 'production') {
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <PagesLinks />
+            <PagesRouter />
+        </div>
+    </BrowserRouter>
+, document.getElementById('container') );
